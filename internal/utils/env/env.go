d37bc59d399fce580eb43864d22abf061302bdb2
jcserv <jarrod.servilla@gmail.com>
2025-01-25 21:34:33 -0500

    tweak: additional env funcs

--- internal/utils/env/env.go
+++ internal/utils/env/env.go
@@ -8,3 +8,17 @@ func GetString(key, fallback string) string {
 	}
 	return fallback
 }
+
+func GetBool(key string, fallback bool) bool {
+	if value, ok := os.LookupEnv(key); ok {
+		return value == "true"
+	}
+	return fallback
+}
+
+func GetBytes(key string, fallback []byte) []byte {
+	if value, ok := os.LookupEnv(key); ok {
+		return []byte(value)
+	}
+	return fallback
+}
